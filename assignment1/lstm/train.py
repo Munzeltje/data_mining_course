d4ebf79defe955142c5bdf643cdc7c594a7acbcb
cynetics <brycargue@gmail.com>
2021-04-15 19:38:25 +0200

    removed old code and left out relevant stuff

--- assignment1/lstm/train.py
+++ assignment1/lstm/train.py
@@ -8,86 +8,113 @@ from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 import torch.optim as optim
 
-from LSTM import Model, KL
-from data.load_data import get_wili_data, get_wili_data_bytes
+from LSTM import Model
+from data.load_data import get_data
 from utils.config import LSTM_config
-from utils.utils import validate_paragraphs
+from utils.utils import validate, validate_set
+
+from sklearn.metrics import classification_report
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train(model, training_loader, validation_loader, validation_data, config, model_name=""):
+def penalty_loss(classifications):
+    classifications = torch.argmax(classifications, dim=2)
+    classifications = classifications.detach().cpu().numpy()
+    for j, batch in enumerate(classifications):
+        seen = 2
+        for i, item in enumerate(batch):
+            newly_seen = item
+            if newly_seen == seen == 1:
+                classifications[j,i] = 2
+            elif newly_seen == seen == 0:
+                classifications[j,i] = 2
+            elif newly_seen == seen == 2:
+                continue
+
+            if newly_seen == 1 and seen == 0:
+                seen = 1
+                continue
+            elif newly_seen == 0 and seen == 1:
+                seen = 0
+                continue
+
+            if newly_seen == 2 and seen == 1:
+                continue
+            elif newly_seen == 2 and seen == 0:
+                continue
+
+            if seen == 2 and newly_seen == 1:
+                seen = 1
+            elif seen == 2 and newly_seen == 0:
+                seen = 0
+
+            elif seen == 2 == newly_seen:
+                continue
+    return torch.tensor(classifications).to(device).long()
+
+def train(model, training_loader, validation_loader, val_data, config, model_name="Crypto_ETH"):
     model.train()
-    optimizer = optim.Adam(model.parameters(), lr=config.lr)
-    criterion = nn.CrossEntropyLoss()
-    kl = KL(divisor=25)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.98)
+    #optimizer = optim.Adam(model.parameters(), lr=config.lr)
+    #optimizer = optim.Adam(model.parameters(), lr=config.lr, betas=(0.99,0.999), weight_decay=5e-4)
+    optimizer = optim.Adam(model.parameters(), lr=config.lr, weight_decay=5e-4)
+    L1 = nn.MSELoss()
+    CE = nn.CrossEntropyLoss(weight=torch.tensor([3,3,1]).float().to(device))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.97)
     avg_train_loss = []
     train_loss = []
-    accuracy = 0
     for epoch in range(config.epochs):
-        print("Starting Epoch: {}".format(epoch))
         for i, (inputs, labels) in enumerate(training_loader):
-            inputs = inputs.to(device)
+            inputs = inputs.to(device).float()
             labels = labels.to(device)
             optimizer.zero_grad()
+            classification, _ = model(inputs)
+            labels_classification = labels[:,:,-2].long()
 
-            output = model(inputs)
-            #log_alpha_1 = model._bayesian1._log_alpha
-            log_alpha_2 = model._bayesian._log_alpha
-            #kl1 = kl(log_alpha_1)
-            kl2 = kl(log_alpha_2)
-            kl_divergence = kl2
+            #price_loss = torch.sqrt(L1(prices.squeeze(2), labels_prices))
+            class_loss = CE(classification.squeeze(2).permute(0,2,1), labels_classification)
+            #class_loss = class_loss #+ 0.5*CE(classification.squeeze(2).permute(0,2,1),penalty_loss(classification))
 
-            loss = criterion(output, labels)
-            #print("Loss: ", loss)
-
-            loss += kl_divergence #/ 5
-            #print("loss: ", loss)
+            #loss = 100*price_loss + class_loss
+            loss = class_loss
             loss.backward()
             optimizer.step()
             train_loss.append(loss.item())
 
-            if  (i+1) % 42 == 0:
-                print('%d iterations' % (i+1))
-                avg = np.mean(train_loss[-50:])
-                avg_train_loss.append(avg)
-                print('Loss: %.3f' % avg)
-                print()
-                #accuracy = validate_paragraphs(model, validation_data, validation_loader, save_classification_report=False)
-                #print("Current Accuracy: {}, After {} iterations in Epoch {}".format(accuracy, i, epoch))
-                #model.train()
-                #torch.save(model.state_dict(), "./models/LSTM/"+model_name+str(epoch)+"_"+str(accuracy)+".pt")
+        if  (epoch+1) % config.print_every == 0:
+            #print("price loss: ",price_loss.item())
+            print("Current Epoch: {}".format(epoch+1))
+
+            avg = np.mean(train_loss[-100:])
+            avg_train_loss.append(avg)
+            print('Loss: %.6f' % avg)
+            print()
+            _ = validate_set(model, val_data, validation_loader, config)
 
         scheduler.step()
-        torch.save(model.state_dict(), "./models/LSTM/"+model_name+str(epoch)+"_"+str(config.batch_size)+"_"+str(config.input)+"_"+str(config.sequence_length)+".pt")
-        #torch.save(model.state_dict(), "./models/LSTM/"+model_name+str(epoch)+"_"+str(accuracy)+".pt")
-    #write_results((avg_train_loss, val_loss, val_accuracy), model_type+"_")
+
+        torch.save(model.state_dict(), "./models/LSTM/"+str(epoch)+"_"+str(config.batch_size)+"_"+str(config.input)+"_"+str(config.sequence_length)+".pt")
+
+        model.train()
+
     print("Iterators Done")
 
 def main():
 
     config = LSTM_config()
 
-    if config.input == 'bytes':
-        # Load Data for bytes
-        training_data, validation_data = get_wili_data_bytes(config)
-    else:
-        # Load Data
-        training_data, validation_data = get_wili_data(config)
-
+    training_data, validation_data = get_data(config)
 
     training_loader = DataLoader(training_data,
                              batch_size=config.batch_size,
                              shuffle=True,
                              drop_last=False)
 
-    validation_loader = DataLoader(validation_data,
+    val_loader = DataLoader(validation_data,
                              batch_size=1,
-                             shuffle=True,
+                             shuffle=False,
                              drop_last=False)
 
-    model = Model(config.input_dim, config.embedding_dim,
-                  config.hidden_dim, config.num_layers, bidirectional=False)
+    model = Model(config.input_dim, config.hidden_dim, config.num_layers, bidirectional=False)
 
     if config.model_checkpoint is not None:
         with open(config.model_checkpoint, 'rb') as f:
@@ -95,7 +122,7 @@ def main():
             model.load_state_dict(state_dict)
             print("Model Loaded From: {}".format(config.model_checkpoint))
     model = model.to(device)
-    train(model, training_loader, validation_loader, validation_data, config, model_name=config.model_name)
+    train(model, training_loader,val_loader, validation_data, config, model_name=config.model_name)
 
 
 if __name__ == '__main__':
